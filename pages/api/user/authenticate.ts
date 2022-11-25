593afa6642f14da1ccfa06bc4287a721a2b2bd03
tth_szabolcs <szabolcslaszlototh@gmail.com>
2022-11-25 09:58:06 +0100

    Update

--- pages/api/user/authenticate.ts
+++ pages/api/user/authenticate.ts
@@ -1,16 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { sign } from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import executeQuery from '../../../components/mysql'
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    const { username, password } = JSON.parse(req.body)
+    const { username, password } = JSON.parse(req.body) // Testpass: 9A613JlK123
     //TODO: Authenticate
 
     //Mock data
     const accountID = 6
     const accountName = "MEDNIGHT"
+
+    const hash = (await executeQuery("SELECT password FROM accounts WHERE username = ?", [username]) as any)
+
+    console.log(hash)
+    const compare = (await bcrypt.compare(password, ""))
+    console.log(compare)
 
     const jwt = sign({ sub: accountID, name: accountName, iat: Math.floor((new Date()).getTime() / 1000) }, process.env["staymta-private-key"]!.toString(), { expiresIn: '1h' })
     res.status(200).json({ jwt, accountID, accountName })
